e33f633c528ee75728d18b1d9876617ff780128c
chorpitman <chorpita@gmail.com>
2015-09-11 23:03:11 +0300

    ййй

--- TkachBlog/src/practices_lesson_5/Party.java
+++ TkachBlog/src/practices_lesson_5/Party.java
@@ -11,7 +11,7 @@ public class Party {
 
 	private final Map<String, String> properties = new HashMap<String, String>();
 
-	/**
+	/** dasdasdasdasdsadasdasd dasdsa ג פûגûפג 
 	 * @return the name
 	 */
 	public String getName() {
