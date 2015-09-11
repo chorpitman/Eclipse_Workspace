f1a58e6e35d688ad95e3109469dd540eb1dfd318
Oleg MacBookPro 15 <chorpita@gmail.com>
2015-09-11 23:31:43 +0300

    1233213131

--- TkachBlog/src/practices_lesson_5/Party.java
+++ TkachBlog/src/practices_lesson_5/Party.java
@@ -11,7 +11,7 @@ public class Party {
 
 	private final Map<String, String> properties = new HashMap<String, String>();
 
-	/** dasdasdasdasdsadasdasd dasdsa Что за ебанный карась?
+	/** Что за ебанный карась?
 	 * @return the name
 	 */
 	public String getName() {
